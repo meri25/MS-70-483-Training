7e497ac5f6f73fbd9a4cdca983f5073edf5f8abd
Meri Takahashi <mertakahashi@tech.softbank.co.jp>
2020-03-04 22:53:56 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS_70_483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
