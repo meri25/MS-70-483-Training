6df98bd5c0acf848ecd04fa2125303026922200a
Meri Takahashi <mertakahashi@tech.softbank.co.jp>
2020-03-05 18:38:43 +0900

    question 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -9,21 +10,38 @@ namespace MS_70_483
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--Q1--");
-            Console.WriteLine(TestIfWebSite("http://www.google.com"));
+            #region q.1
+            // Console.WriteLine("--Q1--");
+            // TestIfWebSite("http://www.google.comhttp://www.googlea.comhttp://www.googleb.com");
+            #endregion
+
+            #region q.2
+            // Console.WriteLine("--Q2--");
+            // OutputLogDebugOrReleaseMode();
+            #endregion
+
+            #region q.3
+            Console.WriteLine("--Q3--");
+            #endregion
+
         }
 
-        public static List<string> TestIfWebSite(string url)
+        private static List<string> TestIfWebSite(string url)
         {
-            const string pattern = @"http://(www\.)?(^\.]+)\.com";
+            // 繰り返される単語の正規表現を定義
+            const string pattern = @"http://(www\.)?([^\.]+)\.com";
+
             List<string> result = new List<string>();
 
+            // pattern にマッチする文字列を探す
             MatchCollection myMatches = Regex.Matches(url, pattern);
 
             // Option.A <Answer>
             result = (List<string>)myMatches.GetEnumerator();
             // Note:
             // MatchCollection.GetEnumeratorメソッドは、コレクションを反復処理する列挙子を返します。
+            // ?-- デバッグするとエラー
+            // System.InvalidCastException: 'Unable to cast object of type 'Enumerator' to type 'System.Collections.Generic.List`1[System.String]'.'
 
             // Option.B
             // result = (List<string>)myMatches.SyncRoot();
@@ -36,6 +54,16 @@ namespace MS_70_483
             //result = (from System.Text.RegularExpressions.Match m in myMatches where !m.Success select m.Value).ToList<string>();
 
             return result;
+        }
+
+        private static void OutputLogDebugOrReleaseMode()
+        {
+            #if (DEBUG)
+                Console.WriteLine("Entering Debug Mode");
+            #else
+                Console.WriteLine("Entering ReleaseMode");
+            #endif
+
         }
     }
 }
