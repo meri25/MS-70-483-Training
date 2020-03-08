228785b72bb49a1f20eb5a8e01ae94015f847dd7
Meri Takahashi <mertakahashi@tech.softbank.co.jp>
2020-03-08 12:04:19 +0900

    Added Question5

---
+++ Question5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MS_70_483
+{
+    class Question5
+    {
+        #region Option.1
+        private static void CovertAmount(float amount)
+        {
+            TransferFunds((double)amount);
+        }
+        private static void TransferFunds(double funds)
+        {
+            Console.WriteLine(funds);
+        }
+        #endregion
+
+        #region Option.2
+        private static void CovertAmount_2(float amount)
+        {
+            TransferFunds_2((decimal)amount);
+        }
+        private static void TransferFunds_2(decimal funds)
+        {
+            Console.WriteLine(funds);
+        }
+        #endregion
+
+        #region Option.3
+        private static void CovertAmount_3(float amount)
+        {
+           // TransferFunds_3(amount);
+        }
+        private static void TransferFunds_3(int funds)
+        {
+            Console.WriteLine(funds);
+        }
+        #endregion
+
+        #region Option.4
+        private static void CovertAmount_4(float amount)
+        {
+            TransferFunds_4((int)amount);
+        }
+        private static void TransferFunds_4(float funds)
+        {
+            Console.WriteLine(funds);
+        }
+        #endregion
+    }
+
+}
--- README.md
+++ README.md
@@ -19,3 +19,17 @@ You need to get the list of all the types defined in the assembly that is being 
 How should you complete the code? To answer, 
 drag the appropriate code elements to the correct targets in the answer area. Each code element may be used once, 
 more than once, or not at all. You may need to drag the split bar between panes or scroll to view content.
+
+# Question.5
+
+You are developing an application that includes methods named ConvertAmount and TransferFunds. 
+You need to ensure that the precision and range of the value in the amount variable is not lost when the TransferFunds() method is called.
+Which code segment should you use?
+
+※ 選択肢は、Question5.cs を参照してください。
+
+Answer:A
+Explanation:
+doubleキーワードは、64ビット浮動小数点値を格納する単純型を示します。
+floatキーワードは、32ビット浮動小数点値を格納する単純型を示します。
+参照：double（C＃リファレンス）
