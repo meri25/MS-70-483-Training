c3231b3721e6c70c463abce2898c8bd19d34a74e
Meri Takahashi <mertakahashi@tech.softbank.co.jp>
2020-03-09 20:58:04 +0900

    Added MS-Guide project

---
+++ MS-Guide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MS_Guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
